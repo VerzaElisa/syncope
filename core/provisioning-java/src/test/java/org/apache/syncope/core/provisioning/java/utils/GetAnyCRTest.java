5e08ba64942dabc9affc0d7de3aeec2d50d6da00
VerzaElisa <elisaverza.gm@gmail.com>
2023-09-10 18:32:12 +0200

    Impostati test senza password getAnyCR

--- core/provisioning-java/src/test/java/org/apache/syncope/core/provisioning/java/utils/GetAnyCRTest.java
+++ core/provisioning-java/src/test/java/org/apache/syncope/core/provisioning/java/utils/GetAnyCRTest.java
@@ -20,6 +20,7 @@
 package org.apache.syncope.core.provisioning.java.utils;
 
 import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 import java.io.UnsupportedEncodingException;
@@ -32,6 +33,7 @@ import org.apache.syncope.common.lib.request.GroupCR;
 import org.apache.syncope.common.lib.request.UserCR;
 import org.apache.syncope.common.lib.request.AnyUR;
 import org.apache.syncope.common.lib.to.ConnObject;
+import org.apache.syncope.common.lib.to.GroupTO;
 import org.apache.syncope.common.lib.types.AnyTypeKind;
 import org.apache.syncope.core.persistence.api.dao.*;
 import org.apache.syncope.core.persistence.api.entity.*;
@@ -61,6 +63,7 @@ import org.identityconnectors.common.security.SecurityUtil;
 import org.identityconnectors.common.security.GuardedByteArray;
 import org.apache.syncope.common.lib.request.AnyCR;
 import org.apache.syncope.common.lib.to.Provision;
+import org.apache.syncope.common.lib.to.AnyObjectTO;
 import org.apache.syncope.common.lib.to.AnyTO;
 import org.apache.syncope.common.lib.to.Item;
 import org.apache.syncope.common.lib.to.UserTO;
@@ -92,7 +95,6 @@ public class GetAnyCRTest {
     private String attrName;
 
     //Vere
-    private AnyTypeKind anyTypeKind;
     private ConnObjectUtils cou;
     private ConnObjectUtils couToSpy;
 
@@ -123,41 +125,51 @@ public class GetAnyCRTest {
     private VirAttrHandler virAttrHandler;
     private VirAttrCache virAttrCache;
     private IntAttrNameParser intAttrNameParser;
-
+    private String type;
+    private String auxClasses;
+    private String realm;
+    private boolean attributes;
+    private boolean password;
+    private AnyTypeKind kind;
+    private String username;
+    private String pass;
+    private boolean mustChangePassword;
+    private boolean generatePass;
+    private String ex;
+    private String name;
+    private String owner;
+    private Set<Attr> attrSet;
 
     @Parameters
     public static Collection<Object[]> getTestParameters(){
         return Arrays.asList(new Object[][]{
-//              | exception             | attrName    |
-                {null                   , "commonObj" },
+//              | type             | auxClasses                  | realm          | attributes | password | kind                   | generatePass |
+                { "testType"       , "testClassOne testClassTwo" , "path/to/realm", true       , false    , AnyTypeKind.USER       , false        },
+                { "testType"       , "testClassOne testClassTwo" , "path/to/realm", true       , false    , AnyTypeKind.GROUP      , false        },
+                { "testType"       , "testClassOne testClassTwo" , "path/to/realm", true       , false    , AnyTypeKind.ANY_OBJECT , false        },
 
         });
     }
 
-    public GetAnyCRTest(String exception, String attrName) {
-        this.exception = exception;
-        this.attrName = attrName;
+    public GetAnyCRTest(String type, String auxClasses, String realm, boolean attributes, boolean password, AnyTypeKind kind, boolean generatePass) {
+        this.type = type;
+        this.auxClasses = auxClasses;
+        this.realm = realm;
+        this.attributes = attributes;
+        this.password = password;
+        this.kind = kind;
+        this.generatePass = generatePass;
     }
 
     @Before
     public void anyCRSetUp() throws UnsupportedEncodingException, ParseException{
-        templateUtils = mock(TemplateUtils.class);
-        realmDAO = mock(RealmDAO.class);
-        userDAO = mock(UserDAO.class);
-        passwordGenerator = mock(PasswordGenerator.class);
-        resourceDAO = mock(ExternalResourceDAO.class);
-        mappingManager = mock(MappingManager.class);
-        anyUtilsFactory = mock(AnyUtilsFactory.class);
-        obj = mock(ConnectorObject.class);
-        pullTask = mock(PullTask.class);
-        anyTypeKind = AnyTypeKind.USER;
-        provision = mock(Provision.class);
-
+        mockGenerator();
         //Viene fatta la spy della classe per ridefinire il comportamento del metodo getAnyTOFromConnObject
         couToSpy = new ConnObjectUtils(templateUtils, realmDAO, userDAO, resourceDAO, passwordGenerator, mappingManager, anyUtilsFactory);
         cou = spy(couToSpy);
-        AnyTO myAnyTO = createAnyTO();
-        myAnyTO.setKey("ella");
+
+        //Stub del metodo getAnyTOFromConnObject
+        AnyTO myAnyTO = myGetAnyTOFromConnObject();
         doReturn(myAnyTO).when(cou).getAnyTOFromConnObject(any(ConnectorObject.class), any(PullTask.class), any(AnyTypeKind.class), any(Provision.class));
     }
 
@@ -165,37 +177,138 @@ public class GetAnyCRTest {
     @Test
     public void anyURTest(){
         try{
-            AnyUR ret = cou.getAnyUR("key", obj, new UserTO(), pullTask, anyTypeKind, provision);
-            assertEquals("key", ret.getKey());
+            AnyCR ret = cou.getAnyCR(obj, pullTask, kind, provision, generatePass);
+            assertEquals(new HashSet<>(Arrays.asList(auxClasses.split(" "))), ret.getAuxClasses());
+            assertEquals(realm, ret.getRealm());
+            switch(kind){
+                case USER:
+                    assertTrue(ret instanceof UserCR);
+                    UserCR userRet = (UserCR) ret;
+                    assertEquals(username, userRet.getUsername());
+                    assertEquals(pass, userRet.getPassword());
+                    assertEquals(mustChangePassword, userRet.isMustChangePassword());
+                    break;
+                case GROUP:
+                    assertTrue(ret instanceof GroupCR);
+                    GroupCR groupRet = (GroupCR) ret;
+                    assertEquals(name, groupRet.getName());
+                    assertEquals(attrSet, groupRet.getPlainAttrs());
+                    break;
+                case ANY_OBJECT:
+                    assertTrue(ret instanceof AnyObjectCR);
+                    AnyObjectCR anyObjectRet = (AnyObjectCR) ret;
+                    assertEquals(name, anyObjectRet.getName());
+                default:
+            }
         }catch(Exception e){
+            ex = e.getClass().getSimpleName();
             e.printStackTrace();
+        }finally{
+            assertNull(ex);
+            verify(cou).getAnyTOFromConnObject(obj, pullTask, kind, provision);
         }
     }
 
+
+
+
     public <T extends AnyTO> T createAnyTO(){
-        T myAnyTO = (T) new UserTO();
-        return myAnyTO;
-    }
+        T myAnyTO = null;
 
-    public <C extends AnyCR> C createAnyCR() {
-        C result = null;
-
-        switch (anyTypeKind) {
+        switch (kind) {
             case USER:
-                result = (C) new UserCR();
+                myAnyTO = (T) new UserTO();
                 break;
 
             case GROUP:
-                result = (C) new GroupCR();
+                myAnyTO = (T) new GroupTO();
                 break;
 
             case ANY_OBJECT:
-                result = (C) new AnyObjectCR();
+                myAnyTO = (T) new AnyObjectTO();
                 break;
 
             default:
         }
+        return myAnyTO;
+    }
 
-        return result;
+    public <C extends AnyCR> C createAnyCR() {
+    C result = null;
+    switch (kind) {
+        case USER:
+            result = (C) new UserCR();
+            break;
+
+        case GROUP:
+            result = (C) new GroupCR();
+            break;
+
+        case ANY_OBJECT:
+            result = (C) new AnyObjectCR();
+            break;
+
+        default:
+    }
+    return result;
+    }
+
+
+    public AnyTO myGetAnyTOFromConnObject(){
+        //Costruzione anyTO con parametri
+        AnyTO myAnyTO = createAnyTO();
+        myAnyTO.setType(type); //Settato solo se l'oggetto è un anyObject
+        myAnyTO.getAuxClasses().addAll(new HashSet<>(Arrays.asList(auxClasses.split(" "))));
+        myAnyTO.setRealm(realm);
+            switch(kind){
+                case USER:
+                    if(attributes){
+                        UserTO myUserTO = (UserTO) myAnyTO;
+                        username = "testUser";
+                        mustChangePassword = false;
+                        myUserTO.setUsername(username);
+                        myUserTO.setMustChangePassword(mustChangePassword);
+                        if(password){
+                            pass = "testPass";
+                            myUserTO.setPassword(pass);
+                        }
+                    }
+                    break;
+                case GROUP:
+                    if(attributes){
+                        GroupTO myGroupTO = (GroupTO) myAnyTO;
+                        attrSet = new HashSet<Attr>();
+                        owner = "testOwner";
+                        name = "testName";
+                        myGroupTO.setName(name);
+                        Attr attrOwner = new Attr();
+                        attrOwner.getValues().add(owner);
+                        attrSet.add(attrOwner);
+                        myGroupTO.getPlainAttrs().add(attrOwner);
+                    }
+                    break;
+                case ANY_OBJECT:
+                    AnyObjectTO myAnyObjectTO = (AnyObjectTO) myAnyTO;
+                    name = "testName";
+                    myAnyObjectTO.setName(name);
+                    break;
+                default:
+            }
+        return myAnyTO;
+    }
+
+    public void mockGenerator(){
+        templateUtils = mock(TemplateUtils.class);
+        realmDAO = mock(RealmDAO.class);
+        userDAO = mock(UserDAO.class);
+        passwordGenerator = mock(PasswordGenerator.class);
+        resourceDAO = mock(ExternalResourceDAO.class);
+        mappingManager = mock(MappingManager.class);
+        anyUtilsFactory = mock(AnyUtilsFactory.class, RETURNS_DEEP_STUBS);
+        when(anyUtilsFactory.getInstance(any(AnyTypeKind.class)).newAnyCR()).thenReturn(createAnyCR());
+        obj = mock(ConnectorObject.class);
+        pullTask = mock(PullTask.class);
+        provision = mock(Provision.class);
+
     }
 }
